4ba45ea489f5b71da7f6bd6f561ed8662367c803
MrCruzzz <mka300246@gmail.com>
2025-04-15 17:50:59 +0300

    Create .eslintrc.js
    Signed-off-by: MrCruzzz <mka300246@gmail.com>

---
+++ docs/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'no-unused-vars': 'warn',
+    'no-console': 'warn'
+  },
+};
