3867f31c614141baaa9517f0bfbb1d530db21120
MrCruzzz <mka300246@gmail.com>
2025-04-16 21:27:36 +0300

    Create cardUtils.js
    Signed-off-by: MrCruzzz <mka300246@gmail.com>

---
+++ docs/cardUtils.js
@@ -0,0 +1,8 @@
+/**
+ * Генерує масив парних карток для гри.
+ * @param {number} count - Кількість пар
+ * @returns {Card[]} - Масив карток
+ */
+function generateCardPairs(count) {
+  // логіка генерації карток
+}
